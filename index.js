89d6d299c87acf3dd5d1cc8ee91f866f07ea4524
VaibhavDhamanage <vaibhavdhamanage>
2023-12-05 10:08:16 +0530

    name section added and localstorage issu fixed

--- index.js
+++ index.js
@@ -4,52 +4,65 @@ var body = document.querySelector("body");
 buttn.addEventListener("click", addinfo);
 
 function addinfo() {
-  var amount = document.getElementById("amount");
-  var info1 = document.createElement("li");
+  var name = document.getElementById("Name");
+  var infoname = document.createElement("li");
   var info1text = document.createElement("li");
+  infoname.appendChild(document.createTextNode(name.value + "  "));
+  info1text.appendChild(document.createTextNode(name.value));
+  name.value = " ";
+  body.appendChild(infoname);
+
+  var amount = document.getElementById("amount");
+  var info1 = document.createElement("span");
   info1.appendChild(document.createTextNode("$" + amount.value + " - "));
-  info1text.appendChild(document.createTextNode(amount.value));
   amount.value = " ";
-  body.appendChild(info1);
+  infoname.appendChild(info1);
 
   var catvalue = document.getElementById("categeory");
   var info3 = document.createElement("span");
   info3.appendChild(document.createTextNode(catvalue.value));
   catvalue.value = " ";
-  info1.appendChild(info3);
+  infoname.appendChild(info3);
 
   var decvalue = document.getElementById("Description");
   var info2 = document.createElement("span");
   info2.appendChild(document.createTextNode(decvalue.value + " "));
   decvalue.value = " ";
-  info1.appendChild(info2);
-
-  let info;
-  localStorage.setItem("info", info1.textContent);
+  infoname.appendChild(info2);
 
   var del = document.createElement("button");
   del.textContent = "Delete";
   del.className = "button";
-  info1.appendChild(del);
+  infoname.appendChild(del);
   del.addEventListener("click", delitem);
 
   var edit = document.createElement("button");
   edit.textContent = " " + "Edit";
   edit.className = "button";
-  info1.appendChild(edit);
+  infoname.appendChild(edit);
   edit.addEventListener("click", edits);
 
+  var newinfo = {
+    name: info1text.textContent,
+    amount: info1text.textContent,
+    category: info3.textContent,
+    description: info2.textContent,
+  };
+  var infodata = JSON.stringify(newinfo);
+  localStorage.setItem("user " + info1text.textContent, infodata);
+
   function edits() {
-    amount.value = info1text.textContent;
+    amount.value = info1.textContent;
     decvalue.value = info2.textContent;
     catvalue.value = info3.textContent;
-    info1.remove();
-    localStorage.removeItem("info");
+    name.value = info1text.textContent;
+    infoname.remove();
+    localStorage.removeItem("user " + info1text.textContent);
   }
 
   function delitem() {
     alert("Are You Sure!");
-    info1.remove();
-    localStorage.removeItem("info");
+    infoname.remove();
+    localStorage.removeItem("user " + info1text.textContent);
   }
 }
